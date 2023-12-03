b46c69dbf4d3420697b9bc8cff421f3a5fc6ecf0
leviobrabo <carlosjunior20313@gmail.com>
2023-12-03 13:53:25 -0300

    Ajuste para envio do presidente

--- fatoshistoricos/database/db.py
+++ fatoshistoricos/database/db.py
@@ -202,6 +202,12 @@ def add_presidentes_db(id, date):
         }
     )
 
+def rem_presidentes_db(date):
+    return db.presidentes.delete_one(
+        {
+            'date': date,
+        }
+    )
 
 def search_id_presidente(id):
     return db.presidentes.find_one({'id': id})
