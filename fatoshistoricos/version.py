162f07a8833aaea75e31bc485d42c6412a2e98d3
leviobrabo <carlosjunior20313@gmail.com>
2023-12-15 22:10:11 -0300

    Alterando a versão

--- fatoshistoricos/version.py
+++ fatoshistoricos/version.py
@@ -3,6 +3,6 @@ from sys import version_info
 import telebot
 
 python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
-fatoshist_version = '2.5.4'
+fatoshist_version = '2.5.5'
 telebot_version = telebot.__version__ if hasattr(
     telebot, '__version__') else "Versão não encontrada"
