80aedf569e13f071a8ba07e17f4e04b29b733bb4
Levi <115052769+leviobrabo@users.noreply.github.com>
2023-12-15 21:48:02 -0300

    Update version.py

--- fatoshistoricos/version.py
+++ fatoshistoricos/version.py
@@ -3,5 +3,5 @@ from sys import version_info
 import telebot
 
 python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
-
+fatoshist_version = '2.5.4'
 telebot_version = telebot.__version__
