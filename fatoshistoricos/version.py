324c2b2c87bf0b48cee0acb19ca4c57338f4cafe
leviobrabo <carlosjunior20313@gmail.com>
2023-12-15 21:58:22 -0300

    [FIX] Telebot version

--- fatoshistoricos/version.py
+++ fatoshistoricos/version.py
@@ -4,4 +4,5 @@ import telebot
 
 python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
 fatoshist_version = '2.5.4'
-telebot_version = telebot.__version__
+telebot_version = telebot.__version__ if hasattr(
+    telebot, '__version__') else "Versão não encontrada"
