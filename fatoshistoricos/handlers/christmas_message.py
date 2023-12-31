d8fd746278acd2b43af04d6ef2820d92a3063110
leviobrabo <carlosjunior20313@gmail.com>
2023-12-31 14:32:20 -0300

    Adicionado a função para enviar mensagem de natal

---
+++ fatoshistoricos/handlers/christmas_message.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from fatoshistoricos.bot.bot import bot
+from fatoshistoricos.config import *
+from fatoshistoricos.database.db import *
+from fatoshistoricos.loggers import logger
+from fatoshistoricos.utils.get_historical import *
+from fatoshistoricos.utils.month import *
+
+
+def christmas_message():
+    try:
+        photo_url = 'https://i.imgur.com/0znRX8g.png'
+
+        caption = f"O canal Hoje na história lhes deseja um feliz natal! 🎊❤️🎉\n\nO Natal é mais que uma comemoração, é uma nova chance que temos de nos reinventarmos e sermos pessoas melhores. Um Feliz e lindo Natal para todos!\n\nE vamos aprender mais informações sobre a história!"
+
+        bot.send_photo(CHANNEL, photo_url, caption=caption)
+
+    except Exception as e:
+
+        logger.error('Erro ao enviar mensagem de natal:', str(e))   
