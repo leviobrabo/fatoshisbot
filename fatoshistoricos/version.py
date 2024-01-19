d82bd1060477bec78bb77062a17dacaca109a360
leviobrabo <carlosjunior20313@gmail.com>
2024-01-19 18:37:27 -0300

    att versão

--- fatoshistoricos/version.py
+++ fatoshistoricos/version.py
@@ -3,7 +3,7 @@ from sys import version_info
 import telebot
 
 python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
-fatoshist_version = '2.5.5'
+fatoshist_version = '2.7.2'
 telebot_version = (
     telebot.__version__
     if hasattr(telebot, '__version__')
